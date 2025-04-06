bf36143484f2cf1f89a200c2212133f418f0a870
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-06 22:57:19 +0900

    Prisma Service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
